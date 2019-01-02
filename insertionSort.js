99eb18ee9e3f41593bf69d40645facba337a1a21
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-01-02 15:55:56 -0500

    insertion sort fixed

---
+++ insertionSort.js
@@ -0,0 +1,12 @@
+const insertionSort = (arr = [358,618,953,105,390,195,60,431,381,543,73,929]) => {
+    for (let i=0; i<arr.length; i+=1) {
+       for (let j=i; j > 0; j-=1) {
+            if (arr[i] < arr[j]) {
+                let element = arr.splice(i,1)
+                arr.splice(j,0,element[0])
+            } 
+       }
+    }
+    return arr
+}
+console.log(insertionSort())
